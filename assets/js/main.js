b3a04ada2eed05904d349b5033b73acfb77ed10c
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-02-11 21:40:57 +0000

    removed external ticker javacript widgets, optimized code in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-//initiate ticker
 
-$('#WebTicker').webTicker();
 
 function getStockData(company, type, cb) { 
     
@@ -22,86 +20,88 @@ function getStockData(company, type, cb) {
     };
 }
 
+function newsArticlesHTML(news) {
+    if (news.length == 0) {
+        return `<div>Currently no news listed</div>`
+    }
+
+    var articleItems = news.map(function (newsItem) {
+
+        var articleDateTime = new Date(newsItem.datetime).toLocaleString("en-US").toString();
+        return `<li><strong>${articleDateTime}</strong> ${newsItem.source}: <a href='${newsItem.url}' target='_blank'>${newsItem.headline}</a></li>`
+    })
+
+    return `<ul>
+                ${articleItems.join('\n')}
+            </ul>`
+}
+
+function createStockChart(data, company) {
+    var graphData = data.chart;
+    var timeLabels = [];
+    var graphDataSet = [];
+
+    graphData.forEach(function (item) {
+        timeLabels.push(item.label);
+        graphDataSet.push(item.close)
+    });
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var stockChart = new Chart(ctx, {
+
+        type: 'line',
+        data: {
+            labels: timeLabels,
+            datasets: [{
+                label: company,
+                backgroundColor: 'rgb(83, 207, 85',
+                borderColor: 'rgb(83, 207, 85)',
+                data: graphDataSet
+            }]
+        },
+
+        // Configuration options go here, empty at this time
+        options: {}
+    });
+}
+
 function stockDataToDocument(company, type) {
 
     getStockData(company, type, function(stockData){
         
-        // fetch stock quote data for table
+        // fetch quote data for summary table
 
-        var company_name = stockData.quote.companyName;
-        var company_symbol = stockData.quote.symbol;
+        $('#company-name').html(stockData.quote.companyName);
+        $('#company-symbol').html(stockData.quote.symbol);
+        
         var latest_price= stockData.quote.latestPrice.toFixed(2);
+        $('#last-price').html(latest_price);
+        
         var price_change = stockData.quote.change.toFixed(2);
+        $('#price-change').html(price_change);
+
         var price_change_percent = stockData.quote.changePercent*100;
-        var latest_time = stockData.quote.latestTime;
-        var last_trade_time = stockData.quote.lastTradeTime;
-        var previous_close = stockData.quote.previousClose.toFixed(2);
+        $('#price-change-percent').html(price_change_percent.toFixed(2));
+
+        $('#latest-time').html(stockData.quote.latestTime);
+        $('#last-trade-time').html(stockData.quote.lastTradeTime);
+        $('#previous-close').html(stockData.quote.previousClose.toFixed(2));
+        
         var market_cap= (stockData.quote.marketCap/Math.pow(10, 9)).toFixed(2);
-        var pe_ratio = stockData.quote.peRatio;
+        $('#market-cap').html(market_cap);
+
+        $('#pe-ratio').html(stockData.quote.peRatio);
+
         var avg_total_volume = stockData.quote.avgTotalVolume.toLocaleString();
-        
-        document.getElementById('company-name').innerHTML = company_name;
-        document.getElementById('company-symbol').innerHTML = company_symbol;
-        document.getElementById('last-price').innerHTML = latest_price;
-        document.getElementById('price-change').innerHTML = price_change;
-        document.getElementById('price-change-percent').innerHTML = price_change_percent.toFixed(2);
-        document.getElementById('latest-time').innerHTML = latest_time;
-        document.getElementById('last-trade-time').innerHTML = last_trade_time;
-        document.getElementById('previous-close').innerHTML = previous_close;
-        document.getElementById('market-cap').innerHTML = market_cap;
-        document.getElementById('pe-ratio').innerHTML = pe_ratio;
-        document.getElementById('avg-total-volume').innerHTML = avg_total_volume;
-        
-        // fetch historic data for graph, 1 month by default
-
-        var graphData = stockData.chart;
-        var timeLabels = [];
-        var graphDataSet = [];
-
-        graphData.forEach(function(item) {
-            timeLabels.push(item.label) });
-        graphData.forEach(function(item) {
-            graphDataSet.push(item.close) });
+        $('#avg-total-volume').html(avg_total_volume);
         
         // create graph with obtained data
-
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var stockChart = new Chart(ctx, {
-
-            type: 'line',
-            data: {
-                labels: timeLabels,
-                datasets: [{
-                    label: company_name,
-                    backgroundColor: 'rgb(83, 207, 85',
-                    borderColor: 'rgb(83, 207, 85)',
-                    data: graphDataSet
-                }]
-            },
-
-            // Configuration options go here, empty at this time
-            options: {}
-        });
+        
+        createStockChart(stockData, company)
 
         // fetch news data from selected stock
 
-        var newsData = stockData.news;
-        var news_ticker = document.getElementById('WebTicker')
+        $('#news-ticker').html(newsArticlesHTML(stockData.news));
 
-        var articleItems = [];
-
-        newsData.forEach(function(item) {
-            var articleDateTime = new Date(item.datetime).toLocaleString();
-            var articleHeadline = item.headline;
-            var articleSource = item.source;
-            var articleURL = item.url;
-
-            articleItems.push(`<li><strong>${articleDateTime}</strong> ${articleSource}: <a href='${articleURL}' target='_blank'>${articleHeadline}</a></li>\n`)
-        })
-        articleItems.unshift(`<li class="ticker-spacer">Latest News</li>\n`);
-        articleItems.push(`<li class="last">Today's Date here</li>\n`);
-        console.log(articleItems);
-        news_ticker.append(articleItems);
-        
     });
 }
