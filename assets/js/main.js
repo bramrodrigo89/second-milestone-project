e0614a57898c0101201ae79b144470f5391d7c95
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-02-12 16:34:49 +0000

    added new buttons over chart to update data on the chart depending on time range of interest

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,10 @@
-
+const sandboxAPI = 'https://sandbox.iexapis.com/'
+const realAPI = 'https://cloud.iexapis.com/'
+const version = 'stable/stock/'
+const testToken= 'token=Tpk_2cb28d1e81034940b4058a5d063b25a5'
+const realToken = 'token=pk_45af954261be4449955cbefadc328b65'
 
 function getStockData(company, type, cb) { 
-    
-    const sandboxAPI = 'https://sandbox.iexapis.com/'
-    const realAPI = 'https://cloud.iexapis.com/'
-    const version = 'stable/stock/'
-    const testToken= 'token=Tpk_2cb28d1e81034940b4058a5d063b25a5'
-    const realToken = 'token=pk_45af954261be4449955cbefadc328b65'
-
     
     var xhr = new XMLHttpRequest();
     xhr.open("GET", sandboxAPI+version+company+'/batch?types='+type+'&range=1m&last=5&'+testToken); 
@@ -46,7 +43,7 @@ function createStockChart(data, company) {
         graphDataSet.push(item.close)
     });
 
-    var ctx = document.getElementById('myChart').getContext('2d');
+    var ctx = document.getElementById('stockChart').getContext('2d');
     var stockChart = new Chart(ctx, {
 
         type: 'line',
@@ -62,7 +59,12 @@ function createStockChart(data, company) {
 
         // Configuration options go here, empty at this time
         options: {}
-    });
+    })
+
+}
+
+function updateStockChart(time) {
+
 }
 
 function stockDataToDocument(company, type) {
@@ -71,37 +73,56 @@ function stockDataToDocument(company, type) {
         
         // fetch quote data for summary table
 
-        $('#company-name').html(stockData.quote.companyName);
-        $('#company-symbol').html(stockData.quote.symbol);
+        var quoteData = stockData.quote;
+
+        $('#company-name').html(quoteData.companyName);
+        $('#company-symbol').html(quoteData.symbol);
         
-        var latest_price= stockData.quote.latestPrice.toFixed(2);
+        var latest_price= quoteData.latestPrice.toFixed(2);
         $('#last-price').html(latest_price);
         
-        var price_change = stockData.quote.change.toFixed(2);
+        var price_change = quoteData.change.toFixed(2);
         $('#price-change').html(price_change);
 
-        var price_change_percent = stockData.quote.changePercent*100;
+        var price_change_percent = quoteData.changePercent*100;
         $('#price-change-percent').html(price_change_percent.toFixed(2));
 
-        $('#latest-time').html(stockData.quote.latestTime);
-        $('#last-trade-time').html(stockData.quote.lastTradeTime);
-        $('#previous-close').html(stockData.quote.previousClose.toFixed(2));
+        $('#latest-time').html(quoteData.latestTime);
+        $('#last-trade-time').html(quoteData.lastTradeTime);
+        $('#previous-close').html(quoteData.previousClose.toFixed(2));
         
-        var market_cap= (stockData.quote.marketCap/Math.pow(10, 9)).toFixed(2);
+        var market_cap= (quoteData.marketCap/Math.pow(10, 9)).toFixed(2);
         $('#market-cap').html(market_cap);
 
-        $('#pe-ratio').html(stockData.quote.peRatio);
+        $('#pe-ratio').html(quoteData.peRatio);
 
-        var avg_total_volume = stockData.quote.avgTotalVolume.toLocaleString();
+        var avg_total_volume = quoteData.avgTotalVolume.toLocaleString();
         $('#avg-total-volume').html(avg_total_volume);
         
         // create graph with obtained data
         
-        createStockChart(stockData, company)
+        createStockChart(stockData, company);
 
         // fetch news data from selected stock
 
         $('#news-ticker').html(newsArticlesHTML(stockData.news));
 
+        $('.update-chart-button').click(function(){
+            var range = this.innerText.toLowerCase();
+
+            var updateXHR = new XMLHttpRequest();
+            updateXHR.open("GET", sandboxAPI+version+company+'/batch?types=chart&range='+range+'&'+testToken); 
+            updateXHR.send(); 
+            updateXHR.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var updatedChartData = JSON.parse(this.responseText);
+                    createStockChart(updatedChartData, company);
+                }
+            };
+            
+        });
+
     });
 }
+
+$(document).ready(stockDataToDocument('AAPL', 'quote,chart,news'));
