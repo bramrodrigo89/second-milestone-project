120e3d27c227f2748d340c07801415bde9dfbce1
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-03-23 22:42:24 +0000

    function to construct table using api started

--- assets/js/watchList.js
+++ assets/js/watchList.js
@@ -9,25 +9,56 @@ if (cachedWatchList == null) {
 // create table using data from watch list array
 
 function watchListTableHTML() {
-    
     var stockRows=[]
-    var tableHeaders = '<tr><th>Name</th><th>Price</th><th>Change</th></tr>'
+    var symbolArray=[]
+
+    if ($('#testAPISwitch').is(':checked')) {
+        var baseURL = testAPI;
+        var keyToken = testToken
+    } else {
+        var baseURL = realAPI;
+        var keyToken = realToken}
 
     if (watchListArray && watchListArray.length) {
-        watchListArray.forEach(function(item){
-            var stockRow=[]
-            var company = item.name
-            stockRow.push(`<th>${company}</th><td>Value 1</td><td>Value 2</td>`);
-            stockRows.push(`<tr>${stockRow}</tr>`);
+        
+        watchListArray.forEach(function(item) {
+            var symbol = item.symbol
+            symbolArray.push(symbol)
         });
-        return  `<table class="table table-striped table-dark">
+        var URLSymbolArray = symbolArray.join(',').toLowerCase()
+
+        $.when(
+            $.getJSON(`${baseURL}${version}market/batch?symbols=${URLSymbolArray}&types=quote&${keyToken}`),
+        ).then(
+            function(response) {
+                var tableData = response
+                
+                for (let elem in tableData) {
+                    var stockObject = tableData[elem]
+                    var companyName = stockObject.quote.companyName
+                    var latestPrice = stockObject.quote.latestPrice
+                    var stockRow=[]
+
+                    stockRow.push(`<th>${companyName}</th><td>${latestPrice}</td><td>Value 2</td>`);
+                    stockRows.push(`<tr>${stockRow}</tr>`);
+                    
+                }
+
+            }, function(errorResponse) {console.log('Error loading data')}
+        );
+        
+        var rowsHTML = Object.keys(stockRows)
+        console.log(stockRows)
+        console.log(rowsHTML)
+        return  `<table class="table table-striped table-dark my-4">
             <thead>
-                ${tableHeaders}
+                <tr><th>Name</th><th>Latest Price</th><th>Change</th></tr>
             </thead>
             <tbody>
                 ${stockRows}
             </tbody>
             </table>`
+        
     } else {
         return `<p>Currently nothing in your Watch List!</p>`
     }
